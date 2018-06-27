198c396073c3d135eafd8de7c46e36b5929bf517
Frank Dondelinger <fdondelinger.work@gmail.com>
2018-06-27 11:42:27 +0100

    Removed different versions to consolidate into one.

--- Package/Version_1_0/EDISON/R/rinvgamma.R
+++
@@ -1,5 +0,0 @@
-rinvgamma <-
-function(n, shape, scale) {
-    return(1 / rgamma(n, shape=shape, scale=1/scale))
-}
-
--- Package/Version_1_0/EDISON/R/sampleSig2.R
+++
@@ -1,7 +0,0 @@
-sampleSig2 <-
-function(y, Px, v0, gamma0) {
-  out = rinvgamma(1, shape=v0/2 + length(y)/2, 
-                  scale = (gamma0 + t(y) %*% Px %*% y)/2)
-  return(out)
-}
-

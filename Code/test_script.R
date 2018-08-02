415532302e4b1bd5669260f5c8087c470a65eb7a
Frank Dondelinger <f.dondelinger@lancaster.ac.uk>
2018-08-02 09:50:13 +0100

    Updated test script with more detailed example.

--- Code/test_script.R
+++ Code/test_script.R
@@ -2,16 +2,23 @@
 library(EDISON)
 
 set.seed(10)
-# Generate some repeated samples, no changepoints
+
+# Generate a network with 5 nodes and no changepoints
 test = generateNetwork(l=10, q=5, k_bar=0)
 
+# Generate 100 repeated measurements for 10 time points
 test.data = lapply(1:100, simulateNetwork, l=10, net=test)
 
-# Make array and put elements in the right order
+# Make array and put dimensions in the right order (repeats, time points, variables)
 test.data.array = sapply(test.data, function(x) x$sim_data, simplify='array')
 test.data.array = aperm(test.data.array, c(3,2,1))
 
+# Specify inference with no changepoints
 edison.options = defaultOptions()
 edison.options$cp.fixed = TRUE
 
-edison.test = EDISON.run(test.data.array[1:2,,], num.iter=10000, options=edison.options)
+# Run EDISON
+edison.test = EDISON.run(test.data.array, num.iter=100000, options=edison.options)
+
+# Calculate posterior probabilities of the edges in the network
+calculateEdgeProbabilities(edison.test)$probs.segs

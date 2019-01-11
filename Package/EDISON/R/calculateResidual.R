a154d07fb825b50b49a7ac8b208e3a8954af291f
DI LU <dilu@vlan-2678-10-17-66-143.staff.wireless.sydney.edu.au>
2019-01-11 12:33:51 +1100

    Rollback to the non-cpp implementation

--- Package/EDISON/R/calculateResidual.R
+++ Package/EDISON/R/calculateResidual.R
@@ -12,16 +12,8 @@ calculateResidual <- function(x, y, delta2) {
   if (!is.matrix(y)) {
     y = as.matrix(y)
   }
-  
-  return(cpp_calculate_residual(x, y, delta2))
+
+  L = t(y) %*% x;
+
+  return (t(y) %*% y - (delta2/(delta2+1)) %*% L %*% chol2inv(chol(crossprod(x))) %*% t(L))  
 }
-
-
-Rcpp::cppFunction('
-arma::mat cpp_calculate_residual(arma::mat &x, arma::mat &y, double delta2) {
-  arma::mat L = y.t() * x;
-  arma::mat xcross = x.t() * x;
-  xcross.diag() += 1e-7;
-
-  return y.t()*y - (delta2/(delta2 + 1)) * L * inv_sympd(xcross) * L.t();
-}', depends = 'RcppArmadillo')

d13c8ab2a9c5260ae4af8d8b77691eed4e23eb33
Di Lu <di.lu@sydney.edu.au>
2019-01-30 10:56:51 +1100

    Now with C++ enhancement again

--- Package/EDISON/R/calculateResidual.R
+++ Package/EDISON/R/calculateResidual.R
@@ -13,7 +13,13 @@ calculateResidual <- function(x, y, delta2) {
     y = as.matrix(y)
   }
 
-  L = t(y) %*% x;
-
-  return (t(y) %*% y - (delta2/(delta2+1)) %*% L %*% chol2inv(chol(crossprod(x))) %*% t(L))  
+  return(cpp_calculate_residual(x, y, delta2))
 }
+
+Rcpp::cppFunction('
+arma::mat cpp_calculate_residual(arma::mat &x, arma::mat &y, double delta2) {
+  arma::mat L = y.t() * x;
+  arma::mat xcross = x.t() * x;
+  xcross.diag() += 1e-7;
+  return y.t()*y - (delta2/(delta2 + 1)) * L * inv_sympd(xcross) * L.t();
+}', depends = 'RcppArmadillo')
